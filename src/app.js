7a325f9827fabd770ce15e46e4aaf09089a6870e
Bryan Chau <bchau2020@fau.edu>
2025-06-03 01:39:36 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const suits = ['♦', '♥', '♠', '♣']
+  const numValue = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomValue = numValue[Math.floor(Math.random() * numValue.length)];
+   
+  document.getElementById("topSuit").innerHTML = randomSuit;
+  document.getElementById("bottomSuit").innerHTML = randomSuit;
+  document.getElementById("value").innerHTML = randomValue;
+
+  const redSuits = ['♦', '♥']
+  const color = redSuits.includes(randomSuit) ? "red" : "black";
+
+  document.getElementById("topSuit").style.color = color;
+  document.getElementById("bottomSuit").style.color = color;
+  document.getElementById("value").style.color = color;
 };
